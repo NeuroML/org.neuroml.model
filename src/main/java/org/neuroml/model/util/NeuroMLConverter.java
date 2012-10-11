1245bb666095234f82281a89c7ae89c3a384c130
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2012-10-11 10:40:28 +0100

    First commit for NeuroML model API

---
+++ src/main/java/org/neuroml/model/util/NeuroMLConverter.java
@@ -0,0 +1,62 @@
+package org.neuroml.model.util;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.namespace.QName;
+
+import org.neuroml.model.Morphology;
+import org.neuroml.model.Neuroml;
+
+public class NeuroMLConverter
+{
+	protected JAXBContext jaxb;
+	
+	protected Marshaller marshaller;
+	
+	protected Unmarshaller unmarshaller;	
+	
+	
+	public NeuroMLConverter() throws Exception
+	{
+		jaxb = JAXBContext.newInstance("org.neuroml2.model");
+		
+		marshaller = jaxb.createMarshaller();		
+		marshaller.setProperty("com.sun.xml.bind.namespacePrefixMapper",new NeuroMLNamespacePrefixMapper());
+		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT,Boolean.TRUE);
+		
+		unmarshaller = jaxb.createUnmarshaller();
+	}
+	
+
+	
+	public Morphology xmlToMorphology(String xmlFile) throws Exception
+	{
+		File f = new File(xmlFile);
+		if (!f.exists()) throw new FileNotFoundException(f.getAbsolutePath());
+		
+		JAXBElement<Morphology> jbe = (JAXBElement<Morphology>) unmarshaller.unmarshal(f);
+		
+		return jbe.getValue();		
+	}
+	
+
+
+	public void neuroml2ToXml(Neuroml nml2, String filename) throws Exception
+	{
+		JAXBElement<Neuroml> jbc =
+			new JAXBElement<Neuroml>(new QName("neuroml"),
+					                    Neuroml.class,
+					                    nml2);
+		File f = new File(filename);
+		FileOutputStream fos = new FileOutputStream(f);
+
+		marshaller.marshal(jbc, fos);
+		fos.close();
+	}
+}
---
+++ src/test/java/org/neuroml2/model/test/NeuroML2Test.java
@@ -0,0 +1,40 @@
+package org.neuroml2.model.test;
+
+import java.io.File;
+
+import org.junit.Test;
+import org.neuroml.model.ExpTwoSynapse;
+import org.neuroml.model.IzhikevichCell;
+import org.neuroml.model.Neuroml;
+import org.neuroml.model.util.NeuroMLConverter;
+
+
+public class NeuroML2Test
+{
+    
+	@Test public void testCells() throws Exception
+	{
+			Neuroml nml2 = new Neuroml();
+
+            IzhikevichCell iz1 = new IzhikevichCell();
+            nml2.getIzhikevichCell().add(iz1);
+
+            ExpTwoSynapse e2syn = new ExpTwoSynapse();
+            e2syn.setTauRise("2ms");
+            e2syn.setTauDecay("12ms");
+            e2syn.setErev("-10mV");
+            e2syn.setGbase("1nS");
+            nml2.getExpTwoSynapse().add(e2syn);
+
+    		String wdir = System.getProperty("user.dir");
+    		String tempdir = wdir + File.separator + "src/test/resources/tmp";
+            NeuroMLConverter conv = new NeuroMLConverter();
+            String tempFile = tempdir + File.separator + "nml2.xml";
+            conv.neuroml2ToXml(nml2, tempFile);
+
+            System.out.println("Saved to: "+ tempFile);
+            System.out.println("Done NeuroML2Test!");
+    }
+	
+
+}
