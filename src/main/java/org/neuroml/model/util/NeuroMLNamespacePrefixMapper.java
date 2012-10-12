b717bbce6d6352731ce2b03ba920d1184e463bd2
Padraig Gleeson <p.gleeson@gmail.com>
2012-10-12 14:28:16 +0100

    Updating to latest Schema Fixing exported XML's namespace usage so it's valid according to schema

--- src/main/java/org/neuroml/model/util/NeuroMLNamespacePrefixMapper.java
+++ src/main/java/org/neuroml/model/util/NeuroMLNamespacePrefixMapper.java
@@ -5,6 +5,8 @@ import com.sun.xml.bind.marshaller.NamespacePrefixMapper;
 
 public class NeuroMLNamespacePrefixMapper extends NamespacePrefixMapper
 {
+    protected final static String TEMP_NAMESPACE = "_nml2_";
+
 	protected Map<String, String> namespaceToPrefixMap;
 	
 	public NeuroMLNamespacePrefixMapper()
@@ -17,9 +19,11 @@ public class NeuroMLNamespacePrefixMapper extends NamespacePrefixMapper
 		namespaceToPrefixMap.put("http://morphml.org/channelml/schema", "cml");
 		namespaceToPrefixMap.put("http://morphml.org/neuroml/schema", "nml");
 		namespaceToPrefixMap.put("http://morphml.org/networkml/schema", "net");
+		namespaceToPrefixMap.put("http://www.neuroml.org/schema/neuroml2", TEMP_NAMESPACE);
 	}
 	
 	
+    @Override
 	public String getPreferredPrefix(String namespaceUri, String suggestion, boolean requirePrefix)	
 	{
 		if (namespaceToPrefixMap.containsKey(namespaceUri)) {
@@ -27,4 +31,6 @@ public class NeuroMLNamespacePrefixMapper extends NamespacePrefixMapper
 		}
 		return suggestion;
 	}
+
+
 }
