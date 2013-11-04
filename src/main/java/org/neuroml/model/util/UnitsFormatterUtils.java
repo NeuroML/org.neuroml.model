733bd238ac78774897d30e0948e58dfc62cf9890
Padraig Gleeson <p.gleeson@gmail.com>
2013-11-04 19:10:10 +0000

    Improved conversion of units

--- src/main/java/org/neuroml/model/util/UnitsFormatterUtils.java
+++ src/main/java/org/neuroml/model/util/UnitsFormatterUtils.java
@@ -10,11 +10,20 @@ public class UnitsFormatterUtils
 	public static String getFormattedUnits(String unformattedUnit)
 	{
 		String formattedUnit=unformattedUnit;
-		formattedUnit=formattedUnit.replace("_per_","/");
-		formattedUnit=formattedUnit.replace("_","/");
+		formattedUnit=formattedUnit.replace("_per_cm"," cm\u207B");
+		formattedUnit=formattedUnit.replace("_per_ms"," ms\u207B\u00b9");
+		formattedUnit=formattedUnit.replace("_per_mV"," mV\u207B\u00b9");
+		formattedUnit=formattedUnit.replace("_per_m"," m\u207B");
+		formattedUnit=formattedUnit.replace("_per_s"," s\u207B\u00b9");
+        
 		formattedUnit=formattedUnit.replace("2","²");
+		formattedUnit=formattedUnit.replace("3","\u00b3");
+        
 		formattedUnit=formattedUnit.replace("ohm","Ω");
 		formattedUnit=formattedUnit.replace("u","μ");
+		formattedUnit=formattedUnit.replace("_"," ");
+        
+        System.out.println(unformattedUnit+" -> "+ formattedUnit);
 		return formattedUnit;
 	}
 
