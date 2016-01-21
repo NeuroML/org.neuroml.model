c186aee31442769f220cc2778e5d5b4a7714af6d
Padraig Gleeson <p.gleeson@gmail.com>
2016-01-21 09:30:38 +0000

    Adding KS_STATE_COMP_TYPE

--- src/main/java/org/neuroml/model/util/NeuroMLElements.java
+++ src/main/java/org/neuroml/model/util/NeuroMLElements.java
@@ -23,6 +23,7 @@ public class NeuroMLElements {
     public static final String BASE_ION_CHANNEL_COMP_TYPE = "baseIonChannel";
     public static final String ION_CHANNEL_HH_COMP_TYPE = "ionChannelHH";
     public static final String ION_CHANNEL_KS_COMP_TYPE = "ionChannelKS";
+    public static final String KS_STATE_COMP_TYPE = "KSState";
     
     // Cells
     public static final String BASE_CELL_COMP_TYPE = "baseCell";
